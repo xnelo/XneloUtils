ccc6bec827105c56aefcbec207fb4f54e0181921
Spencer Hoffa <s_hoffa@hotmail.com>
2019-08-23 09:51:24 -0600

    Fixed another linux compile error.

--- cpp/include/XneloUtils/Logging/Logging.hpp
+++ cpp/include/XneloUtils/Logging/Logging.hpp
@@ -53,20 +53,20 @@
 #define XNELO_LOGGING_DEBUG3 9
 #define XNELO_LOGGING_DEBUG4 10
 
-#define XNELO_LOG(level, message, ...) XNELO::LOGGING::Log(level, message, __VA_ARGS__);
-#define XNELO_LOG_EMERGENCY(message, ...) XNELO_LOG(XNELO_LOGGING_EMERGENCY, message, __VA_ARGS__);
-#define XNELO_LOG_ALERT(message, ...) XNELO_LOG(XNELO_LOGGING_ALERT, message, __VA_ARGS__);
-#define XNELO_LOG_CRITICAL(message, ...) XNELO_LOG(XNELO_LOGGING_CRITICAL, message, __VA_ARGS__);
-#define XNELO_LOG_ERROR(message, ...) XNELO_LOG(XNELO_LOGGING_ERROR, message, __VA_ARGS__);
-#define XNELO_LOG_WARNING(message, ...) XNELO_LOG(XNELO_LOGGING_WARNING, message, __VA_ARGS__);
-#define XNELO_LOG_NOTICE(message, ...) XNELO_LOG(XNELO_LOGGING_NOTICE, message, __VA_ARGS__);
-#define XNELO_LOG_INFO(message, ...) XNELO_LOG(XNELO_LOGGING_INFO, message, __VA_ARGS__);
-#define XNELO_LOG_INFORMATIONAL(message, ...) XNELO_LOG(XNELO_LOGGING_INFORMATIONAL, message, __VA_ARGS__);
-#define XNELO_LOG_DEBUG(message, ...) XNELO_LOG(XNELO_LOGGING_DEBUG, message, __VA_ARGS__);
-#define XNELO_LOG_DEBUG1(message, ...) XNELO_LOG(XNELO_LOGGING_DEBUG1, message, __VA_ARGS__);
-#define XNELO_LOG_DEBUG2(message, ...) XNELO_LOG(XNELO_LOGGING_DEBUG2, message, __VA_ARGS__);
-#define XNELO_LOG_DEBUG3(message, ...) XNELO_LOG(XNELO_LOGGING_DEBUG3, message, __VA_ARGS__);
-#define XNELO_LOG_DEBUG4(message, ...) XNELO_LOG(XNELO_LOGGING_DEBUG4, message, __VA_ARGS__);
+#define XNELO_LOG(level, message, ...) XNELO::LOGGING::Log(level, message, ##__VA_ARGS__);
+#define XNELO_LOG_EMERGENCY(message, ...) XNELO_LOG(XNELO_LOGGING_EMERGENCY, message, ##__VA_ARGS__);
+#define XNELO_LOG_ALERT(message, ...) XNELO_LOG(XNELO_LOGGING_ALERT, message, ##__VA_ARGS__);
+#define XNELO_LOG_CRITICAL(message, ...) XNELO_LOG(XNELO_LOGGING_CRITICAL, message, ##__VA_ARGS__);
+#define XNELO_LOG_ERROR(message, ...) XNELO_LOG(XNELO_LOGGING_ERROR, message, ##__VA_ARGS__);
+#define XNELO_LOG_WARNING(message, ...) XNELO_LOG(XNELO_LOGGING_WARNING, message, ##__VA_ARGS__);
+#define XNELO_LOG_NOTICE(message, ...) XNELO_LOG(XNELO_LOGGING_NOTICE, message, ##__VA_ARGS__);
+#define XNELO_LOG_INFO(message, ...) XNELO_LOG(XNELO_LOGGING_INFO, message, ##__VA_ARGS__);
+#define XNELO_LOG_INFORMATIONAL(message, ...) XNELO_LOG(XNELO_LOGGING_INFORMATIONAL, message, ##__VA_ARGS__);
+#define XNELO_LOG_DEBUG(message, ...) XNELO_LOG(XNELO_LOGGING_DEBUG, message, ##__VA_ARGS__);
+#define XNELO_LOG_DEBUG1(message, ...) XNELO_LOG(XNELO_LOGGING_DEBUG1, message, ##__VA_ARGS__);
+#define XNELO_LOG_DEBUG2(message, ...) XNELO_LOG(XNELO_LOGGING_DEBUG2, message, ##__VA_ARGS__);
+#define XNELO_LOG_DEBUG3(message, ...) XNELO_LOG(XNELO_LOGGING_DEBUG3, message, ##__VA_ARGS__);
+#define XNELO_LOG_DEBUG4(message, ...) XNELO_LOG(XNELO_LOGGING_DEBUG4, message, ##__VA_ARGS__);
 
 
 namespace XNELO
